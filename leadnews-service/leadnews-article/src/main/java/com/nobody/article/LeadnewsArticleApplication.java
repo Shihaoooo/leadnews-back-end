37243f4a576aed440968a86500f6cdaaee8a84c6
Shihaoooo <3141182493@qq.com>
2025-10-25 17:13:11 +0800

    refactor

--- leadnews-service/leadnews-article/src/main/java/com/nobody/article/LeadnewsArticleApplication.java
+++ leadnews-service/leadnews-article/src/main/java/com/nobody/article/LeadnewsArticleApplication.java
@@ -1,7 +1,6 @@
 package com.nobody.article;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
